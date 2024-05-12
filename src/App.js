368dd7b013d7f9c6e5b41f74c740ff83a5c5cbf1
tayofan <tjdtkd3869@naver.com>
2024-05-12 17:23:18 +0900

    20240512_곽성상_영화앱 클론코딩

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Movie from "./components/Movie";
+import { DUMMY } from "./movieDummy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app-container">
+        {
+          DUMMY.results.map((item) => {
+            return(
+              <Movie 
+                title={item.title}
+                vote_average={item.vote_average}
+                poster_path={item.poster_path}
+              />
+            ) 
+          })
+        }
+
+      </div>
+
     </div>
   );
 }
---
+++ src/movieDummy.js
@@ -0,0 +1,408 @@
+export const DUMMY = {
+    "dates": {
+      "maximum": "2024-05-15",
+      "minimum": "2024-04-03"
+    },
+    "page": 1,
+    "results": [
+      {
+        "adult": false,
+        "backdrop_path": "/iHYh4cdO8ylA3W0dUxTDVdyJ5G9.jpg",
+        "genre_ids": [
+          878,
+          12,
+          28
+        ],
+        "id": 653346,
+        "original_language": "en",
+        "original_title": "Kingdom of the Planet of the Apes",
+        "overview": "진화한 유인원과 퇴화된 인간들이 살아가는 땅. 유인원 리더 프록시무스는 완전한 군림을 위해 인간들을 사냥하며 자신의 제국을 건설한다. 한편, 또 다른 유인원 노아는 우연히 숨겨진 과거의 이야기와 시저의 가르침을 듣게 되고 인간과 유인원이 함께 할 새로운 세상을 꿈꾼다. 어느 날 그의 앞에 나타난 의문의 한 인간 소녀. 노아는 그녀와 함께 자유를 향한 여정을 시작하게 되는데…",
+        "popularity": 2758.182,
+        "poster_path": "/blisxM7EJH9lonN9SAZVTNd2ro8.jpg",
+        "release_date": "2024-05-08",
+        "title": "혹성탈출: 새로운 시대",
+        "video": false,
+        "vote_average": 7.2,
+        "vote_count": 178
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/5cCfqeUH2f5Gnu7Lh9xepY9TB6x.jpg",
+        "genre_ids": [
+          14,
+          12,
+          35
+        ],
+        "id": 967847,
+        "original_language": "en",
+        "original_title": "Ghostbusters: Frozen Empire",
+        "overview": "무더운 여름의 뉴욕의 어느 날, 고대 유물 속 깨어난 데스칠로 인해 정체불명의 냉기가 몰려오고 마침내 도시는 얼어붙고 만다. 유령을 퇴치하는 그루버슨과 라이즈 버스터즈 멤버들은 얼어붙은 세상을 깨부수기 위해 유령 군단을 쫓기 시작하는데…",
+        "popularity": 1801.251,
+        "poster_path": "/mGzSIfzmcf9H91DS06cnka1SYrP.jpg",
+        "release_date": "2024-03-20",
+        "title": "고스트버스터즈: 오싹한 뉴욕",
+        "video": false,
+        "vote_average": 6.696,
+        "vote_count": 580
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/ySgY4jBvZ6qchrxKnBg4M8tZp8V.jpg",
+        "genre_ids": [
+          27,
+          53
+        ],
+        "id": 1111873,
+        "original_language": "en",
+        "original_title": "Abigail",
+        "overview": "오직 돈을 위해서 업계 최고들로만 모인 납치범들. 정체를 알 수 없는 거부의 딸이자 평범한 소녀 ‘애비게일’을 납치하고, 24시간 동안 저택에서 그녀를 감시하고 거액의 돈을 받기로 한다. 하지만 평범한 소녀 ‘애비게일’은 발레리나 뱀파이어였고 오히려 납치범들은 역으로 저택에 갇혀 그녀의 장난감과 먹이가 되었다. 납치범들은 이제 24시간 안에 살기 위해 저택을 탈출해야 한다.",
+        "popularity": 1626.563,
+        "poster_path": "/dRCVYbOjhMNghBQgHuqthG5iLH.jpg",
+        "release_date": "2024-04-18",
+        "title": "애비게일",
+        "video": false,
+        "vote_average": 7.1,
+        "vote_count": 269
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/qrGtVFxaD8c7et0jUtaYhyTzzPg.jpg",
+        "genre_ids": [
+          28,
+          878,
+          12
+        ],
+        "id": 823464,
+        "original_language": "en",
+        "original_title": "Godzilla x Kong: The New Empire",
+        "overview": "고질라 X 콩, 이번에는 한 팀이다! ‘고질라’ VS ‘콩’, 두 타이탄의 전설적인 대결 이후 할로우 어스에 남은 ‘콩’은 드디어 애타게 찾던 동족을 발견하지만 그 뒤에 도사리고 있는 예상치 못한 위협에 맞닥뜨린다. 한편, 깊은 동면에 빠진 ‘고질라’는 알 수 없는 신호로 인해 깨어나고 푸른 눈의 폭군 ‘스카 킹’의 지배 아래 위기에 처한 할로우 어스를 마주하게 된다. 할로우 어스는 물론, 지구상에도 출몰해 전세계를 초토화시키는 타이탄들의 도발 속에서 ‘고질라’와 ‘콩’은 사상 처음으로 한 팀을 이뤄 반격에 나서기로 하는데…",
+        "popularity": 1726.858,
+        "poster_path": "/4z1VMmlxHrziG45901esjB4dpIa.jpg",
+        "release_date": "2024-03-27",
+        "title": "고질라 X 콩: 뉴 엠파이어",
+        "video": false,
+        "vote_average": 6.49,
+        "vote_count": 1038
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/sI6uCeF8mUlZx22mFfHSi9W3XQ9.jpg",
+        "genre_ids": [
+          10749,
+          35
+        ],
+        "id": 843527,
+        "original_language": "en",
+        "original_title": "The Idea of You",
+        "overview": "세상에서 가장 섹시한 보이밴드 어거스트 문의 리드 싱어인 24세 헤이스 캠벨(니컬러스 갈리친)과 40세 싱글 맘 솔렌(앤 해서웨이)이 펼치는 뜻밖의 로맨스에 관한 이야기",
+        "popularity": 1415.826,
+        "poster_path": "/6x9gZWCqEGYObUCJPjTKFT2LNqA.jpg",
+        "release_date": "2024-05-02",
+        "title": "너란 개념",
+        "video": false,
+        "vote_average": 7.519,
+        "vote_count": 590
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+        "genre_ids": [
+          878,
+          12
+        ],
+        "id": 693134,
+        "original_language": "en",
+        "original_title": "Dune: Part Two",
+        "overview": "황제의 모략으로 멸문한 가문의 유일한 후계자 폴. 어머니 레이디 제시카와 간신히 목숨만 부지한 채 사막으로 도망친다. 그곳에서 만난 반란군들과 숨어 지내다 그들과 함께 황제의 모든 것을 파괴할 전투를 준비한다. 한편 반란군들의 기세가 높아질수록 불안해진 황제와 귀족 가문은 잔혹한 암살자 페이드 로타를 보내 반란군을 몰살하려 하는데…",
+        "popularity": 1271.331,
+        "poster_path": "/8uUU2pxm6IYZw8UgnKJyx7Dqwu9.jpg",
+        "release_date": "2024-02-27",
+        "title": "듄: 파트 2",
+        "video": false,
+        "vote_average": 8.208,
+        "vote_count": 3798
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/kYgQzzjNis5jJalYtIHgrom0gOx.jpg",
+        "genre_ids": [
+          16,
+          28,
+          10751,
+          35,
+          14
+        ],
+        "id": 1011985,
+        "original_language": "en",
+        "original_title": "Kung Fu Panda 4",
+        "overview": "마침내 내면의 평화… 냉면의 평화…가 찾아왔다고 믿는 용의 전사 ‘포’ 이젠 평화의 계곡의 영적 지도자가 되고, 자신을 대신할 후계자를 찾아야만 한다. “이제 용의 전사는 그만둬야 해요?” 용의 전사로의 모습이 익숙해지고 새로운 성장을 하기보다 지금 이대로가 좋은 ‘포’ 하지만 모든 쿵푸 마스터들의 능력을 그대로 복제하는 강력한 빌런 ‘카멜레온’이 나타나고 그녀를 막기 위해 정체를 알 수 없는 쿵푸 고수 ‘젠’과 함께 모험을 떠나게 되는데… 포는 가장 강력한 빌런과 자기 자신마저 뛰어넘고 진정한 변화를 할 수 있을까?",
+        "popularity": 1295.469,
+        "poster_path": "/1ZNOOMmILNUzVYbzG1j7GYb5bEV.jpg",
+        "release_date": "2024-03-02",
+        "title": "쿵푸팬더 4",
+        "video": false,
+        "vote_average": 7.122,
+        "vote_count": 1585
+      },
+      {
+        "adult": false,
+        "backdrop_path": null,
+        "genre_ids": [
+          18,
+          10770
+        ],
+        "id": 1219685,
+        "original_language": "fr",
+        "original_title": "Un père idéal",
+        "overview": "",
+        "popularity": 1471.102,
+        "poster_path": "/4xJd3uwtL1vCuZgEfEc8JXI9Uyx.jpg",
+        "release_date": "2024-04-21",
+        "title": "Un père idéal",
+        "video": false,
+        "vote_average": 5,
+        "vote_count": 1
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/4woSOUD0equAYzvwhWBHIJDCM88.jpg",
+        "genre_ids": [
+          28,
+          27,
+          53
+        ],
+        "id": 1096197,
+        "original_language": "en",
+        "original_title": "No Way Up",
+        "overview": "",
+        "popularity": 1244.88,
+        "poster_path": "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
+        "release_date": "2024-01-18",
+        "title": "No Way Up",
+        "video": false,
+        "vote_average": 6.421,
+        "vote_count": 625
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/v5XyXZe8FADw8iHupB4L7QOAwH9.jpg",
+        "genre_ids": [
+          16,
+          35,
+          10751
+        ],
+        "id": 748783,
+        "original_language": "en",
+        "original_title": "The Garfield Movie",
+        "overview": "세상귀찮 집냥이, 바쁘고 험난한 세상에 던져졌다! 집사 ‘존’과 반려견 ‘오디’를 기르며 평화로운 나날을 보내던 집냥이 ‘가필드’. 어느 날, 험악한 길냥이 무리에게 납치당해 냉혹한 거리로 던져진다. 돌봐주는 집사가 없는 집 밖 세상은 너무나도 정신없게 돌아가고 길에서 우연히 다시 만난 아빠 길냥이 ‘빅’은 오히려 ‘가필드’를 위기에 빠지게 하는데… 험난한 세상, 살아남아야 한다!",
+        "popularity": 789.669,
+        "poster_path": "/57g3pHYi3p0JNVO1LkcyYbeMDBf.jpg",
+        "release_date": "2024-04-30",
+        "title": "가필드 더 무비",
+        "video": false,
+        "vote_average": 6.5,
+        "vote_count": 52
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/tpiqEVTLRz2Mq7eLq5DT8jSrp71.jpg",
+        "genre_ids": [
+          878,
+          28,
+          18
+        ],
+        "id": 934632,
+        "original_language": "en",
+        "original_title": "Rebel Moon - Part Two: The Scargiver",
+        "overview": "마더월드의 무자비한 세력에 대항해 전투를 준비하는 반란군들. 서로를 단단히 이어주는 유대가 형성되면서 영웅이 등장하고 전설이 탄생한다.",
+        "popularity": 901.006,
+        "poster_path": "/xqHdyufmN0xzGM116dZReFWVwfy.jpg",
+        "release_date": "2024-04-19",
+        "title": "Rebel Moon — 파트 2: 스카기버",
+        "video": false,
+        "vote_average": 6.104,
+        "vote_count": 710
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/5Eip60UDiPLASyKjmHPMruggTc4.jpg",
+        "genre_ids": [
+          27,
+          9648,
+          53
+        ],
+        "id": 1041613,
+        "original_language": "en",
+        "original_title": "Immaculate",
+        "overview": "신앙심이 깊은 미국인 수녀 세실리아는 이탈리아의 한 외딴 수녀원에서 새로운 여정을 시작하게 된다. 처음엔 따뜻한 환영을 받지만 그 곳에 끔찍한 비밀과 말할 수 없는 공포가 도사리고 있음을 깨닫게 되면서 악몽이 시작되는데...",
+        "popularity": 705.086,
+        "poster_path": "/fdZpvODTX5wwkD0ikZNaClE4AoW.jpg",
+        "release_date": "2024-03-20",
+        "title": "이매큘레이트",
+        "video": false,
+        "vote_average": 6.169,
+        "vote_count": 430
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/lzWHmYdfeFiMIY4JaMmtR7GEli3.jpg",
+        "genre_ids": [
+          878,
+          12
+        ],
+        "id": 438631,
+        "original_language": "en",
+        "original_title": "Dune",
+        "overview": "10191년, 아트레이데스 가문의 후계자인 폴은 시간과 공간을 초월해 과거와 미래를 모두 볼 수 있고, 더 나은 미래를 만들 유일한 구원자인 예지된 자의 운명을 타고났다. 그리고 어떤 계시처럼 매일 꿈에서 아라키스의 행성에 있는 한 여인을 만난다. 귀족들이 지지하는 아트레이데스 가문에 대한 황제의 질투는 폴과 그 일족들을 죽음이 기다리는 아라키스로 이끄는데...",
+        "popularity": 521.781,
+        "poster_path": "/7zV8FTYofAORGm0Umgh1mNNCym8.jpg",
+        "release_date": "2021-09-15",
+        "title": "듄",
+        "video": false,
+        "vote_average": 7.778,
+        "vote_count": 11569
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/2KGxQFV9Wp1MshPBf8BuqWUgVAz.jpg",
+        "genre_ids": [
+          16,
+          28,
+          12,
+          35,
+          10751
+        ],
+        "id": 940551,
+        "original_language": "en",
+        "original_title": "Migration",
+        "overview": "가족을 과잉보호하는 아빠 ‘맥’ 때문에 평생을 작은 연못에서 안전하게 살아온 말러드 가족. 하지만 호기심 가득한 남매 ‘댁스’와 ‘그웬’을 위해 새로운 세상을 모험하고 싶은 엄마 ‘팸’의 설득으로 가족들은 항상 꿈꿔온 자메이카로 생애 첫 가족 모험을 떠나기로 한다! 설렘 넘치는 시작과 달리 태풍을 만나 길을 잃고, 낯선 친구들을 만나고, 위험 가득한 뉴욕에 불시착하게 된다. 인생 처음으로 모든 계획이 틀어지고 위기에 빠진 말러드 가족은 서툴지만 서로를 의지하며 모험을 계속한다. 새로운 세상, 함께라면 두려울 것 없어!",
+        "popularity": 636.327,
+        "poster_path": "/2xE3NI6zElWhwN9WJ92fqbZKmzZ.jpg",
+        "release_date": "2023-12-06",
+        "title": "인투 더 월드",
+        "video": false,
+        "vote_average": 7.481,
+        "vote_count": 1309
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/AuTfo1lbE8s2Y8RVTmlbJAeLWPK.jpg",
+        "genre_ids": [
+          28,
+          53
+        ],
+        "id": 850888,
+        "original_language": "es",
+        "original_title": "Sayen: La cazadora",
+        "overview": "피스크를 혼자 쓰러트릴 수 없다는 것을 깨달은 사옌은 지하 저항 단체와 팀을 이루어 피스크의 무분별한 약탈을 폭로하고 완전히 끝내 버릴 계획을 세운다.",
+        "popularity": 744.176,
+        "poster_path": "/btmngK5iXaxyAEl0ScIWCQG2ITr.jpg",
+        "release_date": "2024-04-26",
+        "title": "사옌 3: 사냥꾼",
+        "video": false,
+        "vote_average": 6.4,
+        "vote_count": 24
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/uv2twFGMk2qBdyJBJAVcrpRtSa9.jpg",
+        "genre_ids": [
+          10752,
+          28,
+          18
+        ],
+        "id": 929590,
+        "original_language": "en",
+        "original_title": "Civil War",
+        "overview": "",
+        "popularity": 536.201,
+        "poster_path": "/sh7Rg8Er3tFcN9BpKIPOMvALgZd.jpg",
+        "release_date": "2024-04-10",
+        "title": "시빌 워",
+        "video": false,
+        "vote_average": 7.416,
+        "vote_count": 630
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/fUC5VsQcU3m6zmYMD96R7RqPuMn.jpg",
+        "genre_ids": [
+          28,
+          80,
+          53
+        ],
+        "id": 1105407,
+        "original_language": "en",
+        "original_title": "Damaged",
+        "overview": "시카고의 한 형사는 새롭게 나타난 연쇄 살인범의 범행이 5년 전 그가 조사했던 사건과 유사하다는 사실을 알게되었고, 그 중 하나가 그의 살해된 여자 친구의 범죄 현장이었다는 것을 알게 되어 스코틀랜드로 향하는데...",
+        "popularity": 719.705,
+        "poster_path": "/u3aPmYOkd8GclXg8nTvKQ4w1f7L.jpg",
+        "release_date": "2024-04-12",
+        "title": "데미지드",
+        "video": false,
+        "vote_average": 4.854,
+        "vote_count": 79
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/pwGmXVKUgKN13psUjlhC9zBcq1o.jpg",
+        "genre_ids": [
+          28,
+          14
+        ],
+        "id": 634492,
+        "original_language": "en",
+        "original_title": "Madame Web",
+        "overview": "우연한 사고로 미래를 볼 수 있게 된 구급대원 '캐시 웹'이 거미줄처럼 엮인 운명을 마주하며, 같은 예지 능력을 가진 적 '심스'에 맞서 세상을 구할 히어로 '마담 웹'으로 거듭나게 되는 과정을 그린 마블의 NEW 히어로 드라마",
+        "popularity": 633.352,
+        "poster_path": "/eqEzpQNusV9XSdnA9HAvlLMeuPs.jpg",
+        "release_date": "2024-02-14",
+        "title": "마담 웹",
+        "video": false,
+        "vote_average": 5.617,
+        "vote_count": 1249
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/j9RQem7mt99c87qML7cBTQ7VrUy.jpg",
+        "genre_ids": [
+          18,
+          10751
+        ],
+        "id": 1079810,
+        "original_language": "en",
+        "original_title": "The Long Game",
+        "overview": "",
+        "popularity": 457.676,
+        "poster_path": "/eVWHzm6bL4ybAuRLqA10Qo7ACfN.jpg",
+        "release_date": "2024-04-12",
+        "title": "The Long Game",
+        "video": false,
+        "vote_average": 5.8,
+        "vote_count": 5
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/fOy2Jurz9k6RnJnMUMRDAgBwru2.jpg",
+        "genre_ids": [
+          16,
+          10751,
+          35,
+          14
+        ],
+        "id": 508947,
+        "original_language": "en",
+        "original_title": "Turning Red",
+        "overview": "엄마의 착한 딸, 아니면 미운 13살? 성적도 우수하고 친구들과 사이도 좋은 데다 부모와도 친밀한 관계를 유지하고 있는, 똑 부러지면서도 엉뚱한 매력의 소유자 메이는 요즘 질풍노도의 시기를 겪느라 고민이 많다. 이 나이에 엄마의 과잉보호를 받자니 스트레스가 이만저만이 아닌 데다, 관심사, 인간관계, 신체 등 그녀의 모든 것이 변하고 있기 때문. 그런데 이 와중에 흥분하면 거대한 레서판다로 변신하는 비밀까지 떠안게 되다니! 아침도 먹어야 하고 학교도 가야 하는데, 이 엄청난 비밀을 어떻게 숨겨야 할까?",
+        "popularity": 423.608,
+        "poster_path": "/hsOhOcX7qDy7bPwbrt1OoITngrf.jpg",
+        "release_date": "2022-03-10",
+        "title": "메이의 새빨간 비밀",
+        "video": false,
+        "vote_average": 7.391,
+        "vote_count": 4939
+      }
+    ],
+    "total_pages": 246,
+    "total_results": 4906
+  } 
